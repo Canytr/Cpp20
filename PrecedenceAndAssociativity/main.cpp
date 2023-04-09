4d4db942de25ca61e331b144e5cdcefd54450edb
Can YETER <cannytr@gmail.com>
2023-04-09 17:36:43 +0300

    added precedence and associativity

--- PrecedenceAndAssociativity/main.cpp
+++ PrecedenceAndAssociativity/main.cpp
@@ -2,25 +2,23 @@
 
 int main(){
     
-    auto var1 {12};
-    auto var2 {13.0};
-    auto var3 {14.0f};
-    auto var4 {15.0l};
-    auto var5 {'e'};
+    int a{6};
+    int b{3};
+    int c{8};
+    int d{9};
+    int e{3};
+    int f{2};
+    int g{5};
+        
+    int result = a + b * c -d/e -f + g; //  6 +  24  -   3 - 2 + 5
     
-    //int modifier suffixes
-    auto var6 { 123u}; // unsigned
-    auto var7 { 123ul}; //unsigned long
-    auto var8 { 123ll}; // long long
+    std::cout << "result : " << result << std::endl;
 
-    std::cout << "var1 occupies : " << sizeof(var1) << " bytes" << std::endl;
-    std::cout << "var2 occupies : " << sizeof(var2) << " bytes" << std::endl;
-    std::cout << "var3 occupies : " << sizeof(var3) << " bytes" << std::endl;
-    std::cout << "var4 occupies : " << sizeof(var4) << " bytes" << std::endl;
-    std::cout << "var5 occupies : " << sizeof(var5) << " bytes" << std::endl;
-    std::cout << "var6 occupies : " << sizeof(var6) << " bytes" << std::endl;
-    std::cout << "var7 occupies : " << sizeof(var7) << " bytes" << std::endl;
-    std::cout << "var8 occupies : " << sizeof(var8) << " bytes" << std::endl;
+    result = a/b*c +d - e + f;  //   16 + 9 - 3 + 2
+    std::cout << "result : " << result << std::endl;
+
+    result = (a + b) * c -d/e -f + g; // 72-3-2+5
+    std::cout << "result () : " << result << std::endl;
    
     return 0;
 }
