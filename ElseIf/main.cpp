b356ed65210cff3a1f3292b4fa10f08abe222836
Can YETER <cannytr@gmail.com>
2023-04-19 23:58:07 +0300

    initial commit

--- ElseIf/main.cpp
+++ ElseIf/main.cpp
@@ -2,13 +2,5 @@
 
 int main()
 {
-    
-    //Compile time error
-    //std::cout << "Hello Space" << std::endl
-    std::cout << "Hello Space" << std::endl;
-  
-    //Runtime error
-    int undefined = 5/0;
-    std::cout << "undefined :" << undefined;
 
 }
