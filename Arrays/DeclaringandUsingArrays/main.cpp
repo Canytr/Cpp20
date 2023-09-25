c1004be4d3ce22c3a7b26f0593b39a768a0ceb7e
Can YETER <cannytr@gmail.com>
2023-09-25 10:54:58 +0300

    declaring and using array

--- Arrays/DeclaringandUsingArrays/main.cpp
+++ Arrays/DeclaringandUsingArrays/main.cpp
@@ -1,35 +1,99 @@
 #include <iostream>
 
 int main(){
+
+    //Declare an array of ints
+    // int scores [10]; // Junk data
+
+    //Read data
+
+    // std:: cout << " scores [0] : " << scores[0] << std::endl;
+    // std:: cout << " scores [1] : " << scores[1] << std::endl;
+   
+
+    //Read with a loop
+    /*
+    for( size_t i {0} ; i < 10 ; ++i){
+        std::cout << "scores [" << i << "] : " << scores[i] << std::endl;
+    }
+    */
+  
+
+
+    /*
+    scores[0] = 20;
+    scores[1] = 21;
+    scores[2] = 22;
+
+    //Print the data out
+    for( size_t i {0} ; i < 10 ; ++i){
+        std::cout << "scores [" << i << "] : " << scores[i] << std::endl;
+    }
+    */
+
+
+    //Write data in a loop
+    /*
+    for(size_t i{0}; i < 10 ; ++i){
+        scores[i] = i * 10;
+    }
+
+    //Print the data out
+    for( size_t i {0} ; i < 10 ; ++i){
+        std::cout << "scores [" << i << "] : " << scores[i] << std::endl;
+    }
+    */
+
+    //Declare and initialize at the same time
+    /*
+	double salaries[5] {12.7, 7.5, 13.2, 8.1, 9.3};
+	
+	for(size_t i{0}; i < 5; ++i){
+		std::cout << "salary[" << i << "] : " << salaries[i] << std::endl;
+	}
+    */
+  
+
+    //If you don't initialize all the elements, those you leave out
+    //are initialized to 0
+    /*
+    int families[5] {12, 7, 5};
+	
+	for(size_t i{0}; i < 5; ++i){
+		std::cout << "families[" << i << "] : " << families[i] << std::endl;
+	}
+    */
     
-    //Addition
-    int number1{2};
-    int number2{7};
 
-    int result = number1 + number2;
-    std::cout << "result : " << result << std::endl;
+   	//Omit the size of the array at declaration
+    /*
+    int class_sizes[] {10,12,15,11,18,17,23,56}; 
+	
+	// Will print this with a range based for loop
+	for(auto value : class_sizes){
+		std::cout << "value : " << value << std::endl;
+	}
+    */
+    
 
-    //Subtraction
-    result = number2 - number1 ;
-    std::cout << "result : " << result << std::endl;
 
-    result = number1 - number2;
-    std::cout << "result : " << result << std::endl;
+   // Read only arrays
+   /*
+   const int birds[] {10,12,15,11,18,17,23,56}; 
+   birds[2] = 8;
+   */
 
-    //Multiplication
-    result = number1 * number2;
-    std::cout << "result : " << result << std::endl;
 
-    //Division
-    result = number2 / number1;
-    std::cout << "result : " << result << std::endl;
-
-    //Modulus
-    result = number2 % number1;                       // 7 % 2
-    std::cout << "result : " << result << std::endl;  // 1
-
-    result = 31 % 10;
-    std::cout << "result : " << result << std::endl;  // 1
-
+    //Sum up scores array, store result in sum
+ 
+    int scores [] {2,5,8,2,5,6,9};
+    int sum {0};
+    
+    for( int element : scores){
+        sum += element;
+    }
+    std::cout << "Score sum : " << sum << std::endl;
+  
+   
     return 0;
 }
